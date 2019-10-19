7f51941604f27fc4577070e14891cd6630190540
samuel zaťovič <szatovic@gmail.com>
2019-10-19 15:28:04 +0200

    Hotový program

---
+++ Open-Lab-02.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Open_Lab_02._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] array1 = { 1.23f, 2.42f, 3.25f, 1,65f, 5,19f};
+            Console.WriteLine(array1[1]);
+        }
+    }
+}
